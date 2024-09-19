10be45d6ff6c88a3938f33daae8439eb98e0f7ed
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-09-19 17:31:31 +0530

    Set admin pane login

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import "./app.css";
-import Sidebar from "./components/sidebar/Sidebar";
-import Topbar from "./components/topbar/Topbar";
-import Home from "./pages/home/Home";
-import NewUser from "./pages/newUser/NewUser";
-import ProductList from "./pages/productList/ProductList";
-import User from "./pages/user/User";
-import UserList from "./pages/userList/UserList";
-import Product from "./pages/product/Product";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NewProduct from "./pages/newProduct/NewProduct";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import { PageRoutes } from "./PageRoutes";
 
 function App() {
+  const rootData = JSON.parse(localStorage.getItem("persist:root"));
+  const user =
+    rootData && rootData.user ? JSON.parse(rootData.user).currentUser : null;
+  const isAdmin = user ? user.isAdmin : false;
   return (
-    <Router>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/users" element={<UserList/>} />
-          <Route path="/user/:userId" element={<User/>} />
-          <Route path="/newUser" element={<NewUser/>} />
-          <Route path="/products" element={<ProductList/>} />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/newProduct" element={<NewProduct/>} />
-
-
-        </Routes>
-      </div>
-    </Router>
+    <Routes>
+      {isAdmin && (
+        <>
+          <Route path="/*" element={<PageRoutes />} />
+        </>
+      )}
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
