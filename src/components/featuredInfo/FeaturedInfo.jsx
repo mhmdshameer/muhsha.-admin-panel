0fd832a537bc93f92d9b7d5ea1522162c3668468
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-09-20 20:02:38 +0530

    Set revenue card

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,35 +1,60 @@
-import { ArrowDownward, ArrowUpward } from "@mui/icons-material"
-import "./featuredInfo.css"
+import { ArrowDownward, ArrowUpward } from "@mui/icons-material";
+import "./featuredInfo.css";
+import { useEffect, useState } from "react";
+import { userRequest } from "../../requestMethods";
 
 const FeaturedInfo = () => {
+  const [income, setIncome] = useState([]);
+  const [perc, setPerc] = useState([]);
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const res = await userRequest.get("orders/income");
+        setIncome(res.data[1].total);
+        setPerc((res.data[1].total * 100) / res.data[0].total - 100);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getIncome();
+  }, []);
   return (
     <div className="featured">
       <div className="featuredItem">
         <span className="featuredTitle">Revanue</span>
         <div className="featuredMoneyContainer">
-            <span className="featuredMoney">$2,415</span>
-            <span className="featuredMoneyRate">-11.4 <ArrowDownward className="featuredIcon negative"/> </span>
+          <span className="featuredMoney">${income}</span>
+          <span className="featuredMoneyRate">
+            {perc}%{" "}
+            {perc > 0 ? (
+              <ArrowUpward className="featuredIcon" />
+            ) : (
+              <ArrowDownward className="featuredIcon negative" />
+            )}{" "}
+          </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
       </div>
       <div className="featuredItem">
         <span className="featuredTitle">Sales</span>
         <div className="featuredMoneyContainer">
-            <span className="featuredMoney">$4,415</span>
-            <span className="featuredMoneyRate">-16.4 <ArrowDownward className="featuredIcon negative"/> </span>
+          <span className="featuredMoney">$4,415</span>
+          <span className="featuredMoneyRate">
+            -16.4 <ArrowDownward className="featuredIcon negative" />{" "}
+          </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
       </div>
       <div className="featuredItem">
         <span className="featuredTitle">Cost</span>
         <div className="featuredMoneyContainer">
-            <span className="featuredMoney">$3,415</span>
-            <span className="featuredMoneyRate">+4.4 <ArrowUpward className="featuredIcon"/> </span>
+          <span className="featuredMoney">$3,415</span>
+          <span className="featuredMoneyRate">+4.4 </span>
         </div>
         <span className="featuredSub">Compared to last month</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FeaturedInfo
+export default FeaturedInfo;
