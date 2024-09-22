66070da646b255b907e64900b44ac04dc8b492dd
Muhammed Shameer <mhmdshameerpm@gmail.com>
2024-09-22 08:36:55 +0530

    Set new product page

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -1,33 +1,73 @@
 import { Publish } from "@mui/icons-material";
 import "./newProduct.css";
+import { useState } from "react";
 
 export default function NewProduct() {
+  const [input, setInput] = useState({});
+  const [file, setFile] = useState(null);
+  const [cat, setCat] = useState([]);
+  console.log(file);
+  const handleChange = (e) => {
+    setInput((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleCat = (e) => {
+    setCat(e.target.value.split(","));
+  };
+
   return (
     <div className="newProduct">
       <h1 className="newProductTitle">New Product</h1>
       <form className="newProductForm">
         <div className="newProductImg">
           <img
-            src="/img/noImage.jpg"
+            src={file ? URL.createObjectURL(file) : "/img/noImage.jpg"}
             alt="product"
             className="newUploadImg"
           />
-          <label for="file">
+          <label htmlFor="file">
             <Publish />
           </label>
-          <input type="file" id="file" style={{ display: "none" }} />
+          <input
+            type="file"
+            id="file"
+            style={{ display: "none" }}
+            onChange={(e) => setFile(e.target.files[0])}
+          />
         </div>
         <div className="newProductDetails">
           <label>Product Name</label>
-          <input type="text" placeholder="Product Name" />
+          <input
+            name="title"
+            type="text"
+            placeholder="Product Name"
+            onChange={handleChange}
+          />
           <label>Description</label>
-          <input type="text" placeholder="Description" />
+          <input
+            name="desc"
+            type="text"
+            placeholder="Description"
+            onChange={handleChange}
+          />
           <label>Category</label>
-          <input type="text" placeholder="Category" />
+          <input
+            name="category"
+            type="text"
+            placeholder="Category"
+            onChange={handleCat}
+          />
           <label>Price</label>
-          <input type="text" placeholder="Price" />
+          <input
+            name="price"
+            type="number"
+            placeholder="Price"
+            onChange={handleChange}
+          />
           <label>stock</label>
-          <select>
+          <select onChange={handleChange}>
             <option value="true">Yes</option>
             <option value="false">No</option>
           </select>
